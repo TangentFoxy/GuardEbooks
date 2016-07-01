c8a42be7f70323914cbffa4be1ac07fb546fb5f8
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-07-01 10:16:07 -0700

    updated stuffs + readme

---
+++ ReadMe.txt
@@ -0,0 +1,13 @@
+What is it?
+
+A simple bot using Markov chains based on a user's timeline to tweet every 15
+minutes a random remix of that user. Yay.
+
+Setup:
+
+1. Install Python / PIP
+   "sudo apt-get install python-pip" or whatever
+2. Install Tweepy / PyMarkovChain
+   "pip install tweepy pymarkovchain"
+3. Copy secret.py.example to secret.py and modify with appropriate info.
+4. "python ./bot.py"
--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ while True:
     corpus = ''
 
     print("Gathering tweets...")
-    for page in tweepy.Cursor(api.user_timeline, screen_name='Guard13007').pages(10):
+    for page in tweepy.Cursor(api.user_timeline, screen_name=secret.SOURCE_USER).pages(secret.PAGE_COUNT):
       for item in page:
         text = item.text.replace(".", "%2E")
         if len(corpus) == 0:
@@ -33,23 +33,20 @@ while True:
     for x in xrange(9):
       print("Tweet " + str(x) + " of tweet loop. (max=9)")
       status = chain.generateString()
-      print('Tweet created: "' + status + '".')
-      #if status.find("https://t") > -1:
-        #status = status.replace("https://t", "https://t.co/8VjFsBJsUA")
-      #status = status.replace("http://t", "http://t.co/8VjFsBJsUA")
+      print('Tweet created: "' + status + '"')
+
       status = status.replace("%2E", ".")
       status = status.replace("&amp;", "&")
       status = status.replace("&lt;", "<")
       status = status.replace("&gt;", ">")
-      status = status.replace("@", "#")      # changed to hashtags instead of spacing out @'s
-        #print('Shortened link auto-completed to Patreon video: "' + status " + '".')
-      # shitty fix
-      status = status.replace("twitter.com", "abc.xyz")                # why did I do this?
-      # fix beginning of link (what the hell is this comment about?)
+      status = status.replace("@", "")                    # cutting out all @'s entirely
+      status = status.replace("twitter.com", "abc.xyz")   # why did I do this?
       print('Tweet modified to "' + status + '".')
+
       if len(status) > 140:
         status = status[0:136] + '...'
-        print('Tweet shortened to: "' + status + '".')
+        print('Tweet shortened to: "' + status + '"')
+
       print("Sending tweet.")
       api.update_status(status=status)
       print("Sleeping for 15 minutes...")
